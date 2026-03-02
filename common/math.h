5e05344a8eb60203eb016b8105f3e102bd62e866
Donovan Watteau <contrib@dwatteau.fr>
2026-03-02 13:03:11 +0100

    COMMON: Undef 'LT' macro in intLog2() implementation for older compilers
    Avoid keeping a temporary two-character macro defined in a header file,
    in case it'd cause some subtle breakage somewhere.

--- common/math.h
+++ common/math.h
@@ -95,6 +95,7 @@ static const char LogTable256[256] = {
 	-1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
 	LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
 	LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
+#undef LT
 };
 
 inline int intLog2(uint32 v) {
